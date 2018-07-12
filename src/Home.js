d38578f55b39e1a7b838e8a2ae1947b26810b553
Javier Candalaft <jcandalaft@elementum.com>
2018-07-11 22:50:55 -0300

    fixes

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import styled from 'styled-components';
 import Header from './common/Header';
 import Content from './common/Content';
 
-const Message = styled.p`
-  color: black;
-  text-decoration: none;
-`;
-
 const MapLink = styled.a`
   font-size: 24px;
   color: #1C9EBD;
@@ -18,10 +13,9 @@ const Home = () => (
   <div>
     <Header>Bienvenido a hot chipa</Header>
     <Content>
-      <Message>
-        <span>Te avisaremos cuando los chipás estén listos.</span>
-        <a href="https://goo.gl/maps/ZXCh4M8ry1s">Click acá para ponerte en camino.</a>
-      </Message>
+      <p>
+        Te avisaremos cuando los chipás estén. Click acá para ponerte en camino.
+      </p>
       <MapLink href="https://goo.gl/maps/ZXCh4M8ry1s">https://goo.gl/maps/ZXCh4M8ry1s</MapLink>
     </Content>
   </div>);
